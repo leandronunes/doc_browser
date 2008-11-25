e999a4e23c2b071162a8f284c3a48a67c85d3d87
Leandro Nunes dos Santos <leandronunes@safernet.org.br>
2008-11-25 13:01:59 -0300

    upgrading plugin for rails 2.2

--- app/controllers/doc_controller.rb
+++ app/controllers/doc_controller.rb
@@ -3,7 +3,7 @@ require 'doc_browser'
 # Controller for serving documentation installed in a Rails application
 class DocController < ApplicationController
 
-  self.view_paths = File.join(File.dirname(__FILE__), '..', 'views')
+  self.view_paths = File.join(File.dirname(__FILE__), '..', '..', 'views')
 
   layout 'doc'
 
--- init.rb
+++ init.rb
@@ -1,6 +1,8 @@
 if ENV['RAILS_ENV'] == 'development'
-  controllers_path = File.join(File.dirname(__FILE__), 'controllers')
-  $LOAD_PATH << controllers_path
-  Dependencies.load_paths << controllers_path
-  config.controller_paths << controllers_path
+  path = File.join(File.dirname(__FILE__), 'app', 'controllers')
+  $LOAD_PATH << path
+  ActiveSupport::Dependencies.load_paths << path
+  ActiveSupport::Dependencies.load_once_paths.delete(path)
 end
+
+
